ecbf6439a5dc7f39859330a812739a81c259bac5
Enginecrafter77 <hutiramichal@gmail.com>
2025-02-13 23:12:13 +0100

    fix: NaturalVoxelIndexer: fromIndex algorithm correction

--- src/main/java/dev/enginecrafter77/livelyrealms/structure/NaturalVoxelIndexer.java
+++ src/main/java/dev/enginecrafter77/livelyrealms/structure/NaturalVoxelIndexer.java
@@ -8,14 +8,16 @@ public class NaturalVoxelIndexer implements VoxelIndexer {
 
     public NaturalVoxelIndexer(Vector3ic space)
     {
+        if(space.x() == 0 || space.y() == 0 || space.z() == 0)
+            throw new IllegalArgumentException("An indexer space cannot have a 0 dimension!");
         this.space = space;
     }
 
     @Override
     public int toIndex(Vector3ic position)
     {
-        if(position.x() > this.space.x() || position.z() > this.space.z() || position.y() > this.space.y())
-            throw new IllegalArgumentException("Position out of space bounds");
+        if(position.x() >= this.space.x() || position.z() >= this.space.z() || position.y() >= this.space.y() || position.x() < 0 || position.y() < 0 || position.z() < 0)
+            throw new IllegalArgumentException(String.format("Position (%d,%d,%d) out of space bounds (0,0,0)-(%d,%d,%d)", position.x(), position.y(), position.z(), this.space.x(), this.space.y(), this.space.z()));
         int row = this.space.x();
         int floor = row * this.space.z();
         return position.y() * floor + position.z() * row + position.x();
@@ -24,11 +26,20 @@ public class NaturalVoxelIndexer implements VoxelIndexer {
     @Override
     public void fromIndex(int index, Vector3i out)
     {
-        int floor = index / this.space.y();
-        index %= this.space.y();
-        int row = index / this.space.z();
-        index %= this.space.z();
+        int rowSize = this.space.x();
+        int floorSize = rowSize * this.space.z();
+
+        int floor = index / floorSize;
+        index %= floorSize;
+        int row = index / rowSize;
+        index %= rowSize;
         out.set(index, floor, row);
+    }
+
+    @Override
+    public int volume()
+    {
+        return this.space.x() * this.space.y() * this.space.z();
     }
 
     public static NaturalVoxelIndexer in(Vector3ic space)
--- src/main/java/dev/enginecrafter77/livelyrealms/structure/VoxelIndexer.java
+++ src/main/java/dev/enginecrafter77/livelyrealms/structure/VoxelIndexer.java
@@ -6,4 +6,5 @@ import org.joml.Vector3ic;
 public interface VoxelIndexer {
     public int toIndex(Vector3ic position);
     public void fromIndex(int index, Vector3i out);
+    public int volume();
 }
