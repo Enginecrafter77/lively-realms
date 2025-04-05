1ed4d15f7cb3d5911288c86fc8c8598a8caf9c0d
Enginecrafter77 <hutiramichal@gmail.com>
2025-04-05 21:15:24 +0200

    fix: Respect the ignoreBlock directive

--- src/main/java/dev/enginecrafter77/livelyrealms/generation/loader/ExpressionSetFacade.java
+++ src/main/java/dev/enginecrafter77/livelyrealms/generation/loader/ExpressionSetFacade.java
@@ -3,12 +3,23 @@ package dev.enginecrafter77.livelyrealms.generation.loader;
 import dev.enginecrafter77.livelyrealms.generation.expression.MultiblockExpression;
 import dev.enginecrafter77.livelyrealms.generation.expression.SymbolExpression;
 import dev.enginecrafter77.livelyrealms.generation.expression.SymbolExpressionRegistry;
+import dev.enginecrafter77.livelyrealms.generation.plan.BuildPlan;
+import dev.enginecrafter77.livelyrealms.generation.plan.ClearAreaForStructurePlan;
+import dev.enginecrafter77.livelyrealms.generation.plan.SimpleStructureBuildPlan;
+import dev.enginecrafter77.livelyrealms.generation.plan.StagedBuildPlan;
+import dev.enginecrafter77.livelyrealms.structure.LitematicaStructureLoader;
 import dev.enginecrafter77.livelyrealms.structure.Structure;
 import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.NotNull;
 
 import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
 
 public class ExpressionSetFacade {
+	private static final LitematicaStructureLoader LOADER = new LitematicaStructureLoader();
+
 	private final SymbolExpressionRegistry.SymbolExpressionRegistryBuilder builder;
 	@Nullable
 	private ResourceLocation ignoredBlock;
@@ -34,6 +45,19 @@ public class ExpressionSetFacade {
 		return new ExpressionFacade(symbol);
 	}
 
+	public SymbolExpression structure(String path)
+	{
+		try
+		{
+			Structure structure = LOADER.load(new File(path).toPath());
+			return new StructureExpression(structure);
+		}
+		catch(IOException exc)
+		{
+			throw new RuntimeException(exc);
+		}
+	}
+
 	public class ExpressionFacade
 	{
 		private final String symbol;
@@ -51,6 +75,37 @@ public class ExpressionSetFacade {
 		public void using(Structure structure)
 		{
 			this.using(MultiblockExpression.of(structure, ignoredBlock));
+		}
+	}
+
+	private class StructureExpression implements SymbolExpression
+	{
+		private final Structure struct;
+		private final BuildPlan plan;
+
+		public StructureExpression(Structure struct)
+		{
+			this.struct = struct;
+			this.plan = this.createPlan();
+		}
+
+		private BuildPlan createPlan()
+		{
+			return StagedBuildPlan.of(new ClearAreaForStructurePlan(this.struct, this::isExplicitBlock), new SimpleStructureBuildPlan(this.struct, this::isExplicitBlock));
+		}
+
+		@NotNull
+		@Override
+		public BuildPlan getBuildPlan()
+		{
+			return this.plan;
+		}
+
+		private boolean isExplicitBlock(BlockState state)
+		{
+			if(ignoredBlock == null)
+				return true;
+			return !state.getBlockHolder().getRegisteredName().equals(ignoredBlock.toString());
 		}
 	}
 }
--- src/main/java/dev/enginecrafter77/livelyrealms/generation/loader/GrammarConfigurationScript.java
+++ src/main/java/dev/enginecrafter77/livelyrealms/generation/loader/GrammarConfigurationScript.java
@@ -2,20 +2,12 @@ package dev.enginecrafter77.livelyrealms.generation.loader;
 
 import dev.enginecrafter77.livelyrealms.generation.GenerationProfile;
 import dev.enginecrafter77.livelyrealms.generation.Grammar;
-import dev.enginecrafter77.livelyrealms.generation.expression.MultiblockExpression;
 import dev.enginecrafter77.livelyrealms.generation.expression.SymbolExpressionRegistry;
-import dev.enginecrafter77.livelyrealms.structure.LitematicaStructureLoader;
-import dev.enginecrafter77.livelyrealms.structure.Structure;
 import groovy.lang.Closure;
 import groovy.lang.DelegatesTo;
 import groovy.lang.Script;
 
-import java.io.File;
-import java.io.IOException;
-
 public abstract class GrammarConfigurationScript extends Script {
-	private static final LitematicaStructureLoader LOADER = new LitematicaStructureLoader();
-
 	private final Grammar.GrammarBuilder grammarBuilder;
 	private final SymbolExpressionRegistry.SymbolExpressionRegistryBuilder expressionRegistryBuilder;
 
@@ -44,18 +36,5 @@ public abstract class GrammarConfigurationScript extends Script {
 	{
 		closure.setDelegate(new ExpressionSetFacade(this.expressionRegistryBuilder));
 		closure.run();
-	}
-
-	public MultiblockExpression structure(String path)
-	{
-		try
-		{
-			Structure structure = LOADER.load(new File(path).toPath());
-			return MultiblockExpression.of(structure);
-		}
-		catch(IOException exc)
-		{
-			throw new RuntimeException(exc);
-		}
 	}
 }
