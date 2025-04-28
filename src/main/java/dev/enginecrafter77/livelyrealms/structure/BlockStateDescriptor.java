e26a25c46891bb0675b861eeafc16337f5b8cbc5
Enginecrafter77 <hutiramichal@gmail.com>
2025-04-28 13:54:15 +0200

    feat: BlockStateDescriptor memoization

--- src/main/java/dev/enginecrafter77/livelyrealms/structure/BlockStateDescriptor.java
+++ src/main/java/dev/enginecrafter77/livelyrealms/structure/BlockStateDescriptor.java
@@ -5,6 +5,7 @@ import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Collection;
 import java.util.Map;
@@ -13,14 +14,17 @@ public class BlockStateDescriptor implements BlockStateProvider {
     private final ResourceLocation name;
     private final Map<String, String> properties;
 
+    @Nullable
+    private BlockState memoizedState;
+
     public BlockStateDescriptor(ResourceLocation name, Map<String, String> properties)
     {
         this.name = name;
         this.properties = properties;
+        this.memoizedState = null;
     }
 
-    @Override
-    public BlockState getBlockState()
+    private BlockState compileState()
     {
         Block block = BuiltInRegistries.BLOCK.get(this.name);
         BlockState state = block.defaultBlockState();
@@ -28,12 +32,25 @@ public class BlockStateDescriptor implements BlockStateProvider {
         for(Property<?> prop : props)
         {
             String value = this.properties.get(prop.getName());
-            state = this.setProperty(state, prop, value);
+            state = setProperty(state, prop, value);
         }
         return state;
     }
 
-    private <T extends Comparable<T>> BlockState setProperty(BlockState state, Property<T> prop, String value)
+    @Override
+    public BlockState getBlockState()
+    {
+        if(this.memoizedState == null)
+            this.memoizedState = this.compileState();
+        return this.memoizedState;
+    }
+
+    public void invalidate()
+    {
+        this.memoizedState = null;
+    }
+
+    private static <T extends Comparable<T>> BlockState setProperty(BlockState state, Property<T> prop, String value)
     {
         T val = prop.getValue(value).orElseThrow();
         return state.setValue(prop, val);
